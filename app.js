19c1aaf39ed639471d01599e9b552cc3fbe651b9
Alexa Carrington <acarring@wellesley.edu>
2020-12-14 11:13:27 -0500

    Added selectors, event listeners, started functions in js

---
+++ app.js
@@ -0,0 +1,20 @@
+const usernameInput = document.querySelector('.username input');
+const passwordInput = document.querySelector('.password input');
+const submit = document.querySelector('.submit button');
+
+//make function to check usernameInput, send alert if reqs not met, send alert if met, clear input
+function checkUsername() {
+
+}
+
+//make function to check passwordInput, send alert if reqs not met, send alert if met, clear input
+function checkPassword() {
+    
+}
+
+//wrap both functions inside one function, which will be called when button is clicked
+function checkInputs() {
+
+}
+
+submit.addEventListener('click', checkInputs);
